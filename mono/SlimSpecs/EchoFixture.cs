bcb923a4add14348188f418586a97eab23b0c182
Gregor Gramlich <gramlich@gramlich-develop.local.gra>
2010-03-09 22:18:34 +0100

    Added fitSharp / Mono

---
+++ mono/SlimSpecs/EchoFixture.cs
@@ -0,0 +1,19 @@
+
+using System;
+
+namespace Fixtures
+{
+	namespace Basics
+	{
+
+		public class EchoFixture
+		{
+			
+			public String EchoString(String value)
+			{
+				return value;
+			}
+		}
+		
+	}
+}
